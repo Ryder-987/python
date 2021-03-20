70356dd641efea5e94fec13bc66d58d2fd0bc0a2
Ryder-987 <ryder6253@gmail.com>
2021-03-20 17:12:46 +0800

    class3

---
+++ class/3/password.py
@@ -0,0 +1,8 @@
+name=input('please input name:')
+password=input('please input password:')
+color=input('please input color:')
+
+if (password=='1234') & (name=='Ryder') & (color=='blue'):
+    print('true')
+else:
+    print('false')
