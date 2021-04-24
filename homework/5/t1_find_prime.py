7871b3c1bfd2b93c8a5726bce07c52d17080f434
Ryder-987 <ryder6253@gmail.com>
2021-04-24 17:43:39 +0800

    class7

--- homework/5/t1_find_prime.py
+++ homework/5/t1_find_prime.py
@@ -1,6 +1,12 @@
-"""
+'''
 輸入一數字n為尋找的區間範圍, 找出區間範圍
 的質數顯示在螢幕上，其餘不顯示。
+'''
 
-
-"""
+n = int (input('n'))
+for i in range(2 , n+1):
+    for j in range(2 ,i):
+        if (i % j == 0):
+            break
+    else:
+        print(i)
